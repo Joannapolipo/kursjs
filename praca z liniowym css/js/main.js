ffccf39067cbb0b4dd33f6737002c28942e6e26d
Piotr Palarz <piotr@depalage.com>
2021-03-05 15:07:33 +0100

    Start

---
+++ praca z liniowym css/js/main.js
@@ -0,0 +1,4 @@
+const container = document.querySelector(".container");
+const h1 = document.querySelector("h1");
+const p = container.querySelectorAll("p");
+const link = container.querySelector("a");
