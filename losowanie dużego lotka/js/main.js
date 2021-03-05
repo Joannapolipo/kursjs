472ea01f9fca4ed82f1225e6fa6bde30320454a3
Piotr Palarz <piotr@depalage.com>
2021-03-05 14:49:31 +0100

    Start

---
+++ losowanie dużego lotka/js/main.js
@@ -0,0 +1,5 @@
+// 1. Znajdź sposób na generowanie losowej liczby
+// 2. Stwórz funkcję, do losowania liczb z podanego przedziału
+// 3. Stwórz funkcję, która wylosuje 6 liczb z przedziału 1-49
+// 4. Wyświetl wylosowane liczby
+// 5. Dodaj mechanizm, który nie pozwoli wylosować drugi raz tej samej liczby
