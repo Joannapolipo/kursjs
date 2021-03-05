5770561582d46e7277940f5d0d2e3ad6ff254a6c
Piotr Palarz <piotr@depalage.com>
2021-03-05 15:15:57 +0100

    Start

---
+++ tooltip/js/main.js
@@ -0,0 +1,7 @@
+// 1. Przypisz zdarzenie mouseenter do wszystkich przekazanych elementów
+// 2. Po najechaniu na element, odczytaj jego pozycję na stronie
+// 3. Stwórz dynamicznie element tooltip
+// 4. Wypozycjonuj element tooltip w odpowiednim miejscu i wstaw go na stronę
+// 5. Przypisz zdarzenie mouseleave do wszystkich przekazanych elementów
+// 6. Po zjechaniu z elementu, usuń tooltip ze strony
+// 7. Zamknij kod w module
