17dde02a56fabf712f3af0f56a39f1c61c69cfbf
Joanna Wirtel <joanna.wirtel@morgansuisse.com>
2024-03-01 08:28:49 +0100

    message

---
+++ tekstowy typ danych/js/main.js
@@ -0,0 +1,20 @@
+const firstName = "Jan";
+const lastName = 'Kowalski';
+
+
+const bookTitle = "\"Władca Pierścieni\"";
+
+console.log(bookTitle);
+
+const multiline = "Linijka pierwsza\nLinijka druga";
+const multilina2 =`Linijka pierwsza
+
+Linijka druga
+
+Linijka trzecia`;
+
+const bookTitleReformed = "Tytuł książki to: " + bookTitle;
+const bookTitleReformed2 = `Tytuł książki to: ${bookTitle}`;
+
+console.log(bookTitleReformed);
+console.log(bookTitleReformed2);
